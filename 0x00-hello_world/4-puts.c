f4690b5d8fb2bc366e44d6cf01623e5aba0698e3
Ermenko-Tesfaye <ermiyas.abera@ju.edu.et>
2022-09-08 17:09:03 +0000

    Files are added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: always 0
+ */
+int main(void)
+{
+	char *txt = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(txt);
+	return (0);
+}
