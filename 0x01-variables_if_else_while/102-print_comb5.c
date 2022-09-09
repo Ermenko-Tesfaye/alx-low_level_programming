cb484e8952353e1f0b733b77df5bf48ac44e5b0c
Ermenko-Tesfaye <ermiyas.abera@ju.edu.et>
2022-09-09 13:21:09 +0000

    Update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,29 +5,40 @@
  */
 int main(void)
 {
-	int num1 = 48, num2 = 48, num3 = 48, num4 = 49;
+	int num1 = 48, num2, num3, num4, tmp1, tmp2;
 
-	while (num1 < 56)
+
+	while (num1 < 57)
 	{
-		while (num2 < 57)
+		num2 = 48;
+		while (num2 < 58)
 		{
-			while (num3 < 58)
+			num3 = 48;
+			while (num3 < 57)
 			{
-				putchar(num1);
-				putchar(num2);
-				putchar(num3++);
-				if (!(num1 == 55 && num2 == 56 && num3 == 58))
+				num4 = 48;
+				while (num4 < 58)
 				{
-					putchar(',');
+					tmp1 = (num1 * 10) + num2;
+					tmp2 = (num3 * 10) + num4;
+					if (tmp1 >= tmp2)
+						continue;
+					putchar(num1);
+					putchar(num2);
 					putchar(' ');
+					putchar(num3);
+					putchar(num4++);
+					if (!(num1 == 57 && num2 == 56 && num3 == 57 && num4 == 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
+				num3++;
 			}
 			num2++;
-			num3 = num2 + 1;
 		}
 		num1++;
-		num2 = num1 + 1;
-		num3 = num1 + 2;
 	}
 	putchar('\n');
 	return (0);
