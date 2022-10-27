58d1a0803a8f731f3d0b4573e518fc56e02be8d1
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-10-27 13:13:27 +0000

    Update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,15 +6,15 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, flag;
+	int i, flag = 0;
 
 	if (n == 0)
 		_putchar('0');
-	for (flag = 0, i = sizeof(n) * 8 - 1; i >= 0; i--)
+	for (i = i(sizeof(n) * 8) - 1; i >= 0; i--)
 	{
 		if ((n >> i) & 1)
 			flag = 1;
 		if (flag == 1)
-			((n >> 1) & 1) ? _putchar('1') : _putchar('0');
+			((n >> i) & 1) ? _putchar('1') : _putchar('0');
 	}
 }
