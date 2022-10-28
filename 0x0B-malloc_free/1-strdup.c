f149ae2f16338e12f40061ec08e935271df3b052
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-10-28 08:43:14 +0000

    If string is NULL it treated as empty string .. added

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,24 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * _strdup - a fun.. returns a pointer to a newly allocated space
  * @str: string to be copy
  * Return: pointer to newly created string
+ */
+char *_strdup(char *str)
+{
+	char *strdup;
+	int i, j;
+
+	if (!str)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		i++;
+	strdup = (char *)malloc(sizeof(char) * i + 1);
+	if (!strdup)
+		return (NULL);
+	for (j = 0; j < i; j++)
+		strdup[j] = str[j];
+	strdup[j] = '\0';
+	return (strdup);
+}
